8560cb4e3ca3cdae906f719a5252aa3fdd5b7daf
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-12 17:34:13 +0500

    parse .env

--- index.mjs
+++ index.mjs
@@ -1,3 +1,20 @@
+// set env
+import { readFile } from "fs/promises";
+
+let err = null;
+const content = await readFile(".env", { encoding: "utf8" }).catch(e => err = e);
+if (err) {
+	console.error(err);
+	process.exit(1);
+}
+
+for (const line of content.trim().split("\n")) {
+	const [key, val] = line.split("=");
+	process.env[key] = val;
+}
+
+console.log(process.env.VK_TOKEN);
+
 import { app } from "#src/app.mjs";
 
 const port = 5000;
